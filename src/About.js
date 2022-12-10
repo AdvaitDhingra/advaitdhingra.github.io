dc203183386e790cf5681e7a682ce4dfab9c70bb
AdvaitDhingra <advait.dhingra1006@gmail.com>
2022-12-10 13:13:07 +0100

    Minor changes

---
+++ src/About.js
@@ -0,0 +1,18 @@
+export default function About(){
+
+    return(
+        <div>
+            <center>
+            <h2>About Me</h2>
+            <p>
+                My name is Advait and I'm a particle physics enthusiast and highschooler from Bonn, Germany. My main interests lie in <br/> <br/>
+                - Theoretical and Experimental Particle Physics <br/>
+                - Quantum Field Theory and String Theory <br/>
+                - The Large Hadron Collider and ATLAS Physics <br/><br/>
+                Feel free to email me about any of these topics :D
+                Email: advait.dhingra1006[at]gmail.com
+            </p>
+            </center>
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import photo from './me.jpeg'
 import linkedin from "./icons8-linkedin-circled.gif"
-import email from './icons8-email-open-50.png'
 import twitter from './icons8-twitter.gif'
 import github from './icons8-github.gif'
+import About from './About';
 
 function App() {
   return (
@@ -16,9 +16,6 @@ function App() {
       <a href = "https://linkedin.com/in/advaitdhingra" target="_blank"><img src = {linkedin} style = {{
         width: "30px"
       }}/></a>
-      <a href = "mailto:advait.dhingra1006@gmail.com"><img src = {email} style = {{
-        width: "30px"
-      }}/></a>
       <a href = "https://twitter.com/advaitdhingra" target="_blank"><img src = {twitter} style = {{
         width: "30px"
       }}/></a>
@@ -26,6 +23,7 @@ function App() {
         width: "30px"
       }}/></a>
       </div>
+      <About/>
     </div>
   );
 }
